451f421238c2b1e7dcc7229e97c4ef580a0b30b2
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-01 18:21:41 -0600

    Solution exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, numbOfTimes) {
+    let repeatedString = ""
+    if(numbOfTimes < 0){
+        return 'ERROR';
+    } else {
+      for (let i = 0; i < numbOfTimes; i++) {
+            repeatedString = repeatedString + word;
+        }
+        return repeatedString;  
+    }
+    
 };
 
 // Do not edit below this line

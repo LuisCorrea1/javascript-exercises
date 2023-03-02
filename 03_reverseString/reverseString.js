bbb200f94cd64a4178de430d68fe4e1fc42c04ce
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-01 18:35:26 -0600

    Solution exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitStr = str.split("");
+    let reverseStr = splitStr.reverse();
+    let reversed = reverseStr.join("");
 
+    return reversed;
 };
 
 // Do not edit below this line

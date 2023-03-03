fbbafc2e9f24fb35b6a4914fdf08745ef2aef194
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-03 16:29:46 -0600

    Solution exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+//The rest parameter allows a function to accept an indefinite number of arguments as an array
+const removeFromArray = function(givenArray, ...values) {
+const newArray = [];
 
+givenArray.forEach(value => {
+    if (!values.includes(value)) {
+        newArray.push(value);
+    }
+});
+    return newArray;
 };
 
 // Do not edit below this line

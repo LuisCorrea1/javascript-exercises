4ab3e1042d42ed4aa8a6fd60f8670331d0c967e9
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-03 17:19:30 -0600

    Solution exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(fromValue, toValue) {
+    //All given values should be numbers
+    if ((typeof fromValue != 'number') || (typeof toValue != 'number')) {
+        return "ERROR";
+    }
+    //Parameters must be positive
+    if ((fromValue < 0) || (toValue < 0)) {
+        return "ERROR";
+    }
+    //Change variables if the first number is greater than the second one
+    if (fromValue > toValue) {
+        const newFromValue = fromValue;
+        fromValue = toValue
+        toValue = newFromValue;
+    }
+    //Sum of the parameterss
+    let sum = 0;
+    for (let i = fromValue; i <= toValue; i++){
+        sum = sum +i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

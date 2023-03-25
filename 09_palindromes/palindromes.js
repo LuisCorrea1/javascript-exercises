6bf83ca24475e826a77e512135643d6f44047cea
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-24 22:28:48 -0600

    Solution exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const newString = string.replace(/[\W_]/g, "").toLowerCase();
+    const reversedString = newString.split("").reverse().join("");
 
+    return newString === reversedString ? true : false;
 };
 
 // Do not edit below this line

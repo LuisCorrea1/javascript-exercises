3926b5514317519dfad53a88e039e3f7c4b3e979
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-24 21:45:16 -0600

    Solution exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let result = a + b;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let result = a - b;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result = (array[i] * result);
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let result = 0;
+  result = Math.pow(a, b);
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+  for (let i = n; i >= 1; i--) {
+    result = result * i;
+  }
+  return result;
 };
 
 // Do not edit below this line
